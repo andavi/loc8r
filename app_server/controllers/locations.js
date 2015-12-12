3f9008896eeaea743a9b13e99463eb5412a6fe26
Andrew Davis <andrew.bradley.davis@gmail.com>
2015-12-12 13:15:56 -0500

    Move data from location-info view to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -24,14 +24,53 @@ module.exports.homelist = function(req, res) {
       address: '44c4 Yakisuba Way, Night City, Sprawl s.4X84',
       rating: 1,
       facilities: ['Hot drinks', 'Hot noodles', 'Metaverse access', 'Microsofts'],
-      distance: '100m'
+      distance: '50ft'
     }]
   });
 };
 
 /* GET 'Location info' page */
 module.exports.locationInfo = function(req, res) {
-  res.render('location-info', {title: 'Location info'});
+  res.render('location-info', {
+    title: 'Location info',
+    pageHeader: {title: 'Starchunks'},
+    sidebar: {
+      context: 'is on Loc8r because if has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: {
+      name: 'Starchunks',
+      address: '125 High Street, Raleigh, NC 27601',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      coords: {lat: 51.455041, lng: -0.9690884},
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      },{
+        days: 'Saturday',
+        opening: '8:00pm',
+        closing: '5:00pm',
+        closed: false
+      },{
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Andrew Davis',
+        rating: 5,
+        timestamp: '16 July 2015',
+        reviewText: 'What a great place. I can\'t say enough good things about it.\n5 stars!'
+      },{
+        author: 'Henry Case',
+        rating: 1,
+        timestamp: '13 January 2097',
+        reviewText: 'Consumerist refuse at its worst. Coffee tastes like bile.'
+      }]
+    }
+  });
 };
 
 /* GET 'Add review' page */
