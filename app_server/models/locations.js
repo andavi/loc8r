073976aae7f3a071d448b12f453c1eb5b6bb6139
Andrew Davis <andrew.bradley.davis@gmail.com>
2015-12-16 06:14:00 -0500

    Compile location schema

--- app_server/models/locations.js
+++ app_server/models/locations.js
@@ -1,17 +1,17 @@
 var mongoose = require('mongoose');
 
-var openingTimeSchema = new mongoose.Schema({
-  days: {type: String, required: true},
-  opening: String,
-  closing: String,
-  closed: {type: Boolean, require: true}
-});
-
 var reviewSchema = new mongoose.Schema({
   author: String,
   rating: {type: Number, required: true, min: 0, max: 5},
   reviewText: String,
   createdOn: {type: Date, 'default': Date.now}
+});
+
+var openingTimeSchema = new mongoose.Schema({
+  days: {type: String, required: true},
+  opening: String,
+  closing: String,
+  closed: {type: Boolean, require: true}
 });
 
 var locationSchema = new mongoose.Schema({
@@ -23,3 +23,5 @@ var locationSchema = new mongoose.Schema({
   openingTimes: [openingTimeSchema],
   reviews: [reviewSchema]
 });
+
+mongoose.model('Location', locationSchema);
