8e4658dc863e8f8fbb9728ebaf7a88b13cac7679
Andrew Davis <andrew.bradley.davis@gmail.com>
2015-12-19 09:32:28 -0500

    Fix typo in api router

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -12,8 +12,8 @@ router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
 
 // reviews
 router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-router.get('locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
+router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
+router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
