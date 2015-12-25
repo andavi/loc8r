a963f07821874d5bd554d468a08e1a39ba7dc954
Andrew Davis <andrew.bradley.davis@gmail.com>
2015-12-25 11:45:14 -0500

    Add PUT methods to API

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -6,6 +6,30 @@ var sendJsonResponse = function(res, status, content) {
   res.json(content);
 };
 
+var doAddReview = function(req, res, location) {
+  if (!location) {
+    sendJsonResponse(res, 400, {
+      message: 'locationid not found'
+    });
+  } else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    });
+    location.save(function(err, location) {
+      var thisReview;
+      if (err) {
+        sendJsonResponse(res, 400, err);
+      } else {
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length - 1];
+        sendJsonResponse(res, 201, thisReview);
+      }
+    });
+  }
+};
+
 var updateAverageRating = function(locationid) {
   Loc
     .findById(locationid)
@@ -34,30 +58,6 @@ var doSetAverageRating = function(location) {
         console.log(err);
       } else {
         console.log('Average rating updated to', ratingAverage);
-      }
-    });
-  }
-};
-
-var doAddReview = function(req, res, location) {
-  if (!location) {
-    sendJsonResponse(res, 400, {
-      message: 'locationid not found'
-    });
-  } else {
-    location.reviews.push({
-      author: req.body.author,
-      rating: req.body.rating,
-      reviewText: req.body.reviewText
-    });
-    location.save(function(err, location) {
-      var thisReview;
-      if (err) {
-        sendJsonResponse(res, 400, err);
-      } else {
-        updateAverageRating(location._id);
-        thisReview = location.reviews[location.reviews.length - 1];
-        sendJsonResponse(res, 201, thisReview);
       }
     });
   }
@@ -133,7 +133,53 @@ module.exports.reviewsReadOne = function(req, res) {
 };
 
 module.exports.reviewsUpdateOne = function(req, res) {
-  sendJsonResponse(res, 200, {'status': 'success'});
+  if (!req.params.locationid || !req.params.reviewid) {
+    sendJsonResponse(res, 404, {
+      message: 'Not found, locationid and reviewid are both required'
+    });
+    return;
+  }
+  Loc
+    .findById(req.params.locationid)
+    .select('reviews')
+    .exec(
+      function(err, location) {
+        var thisReview;
+        if (!location) {
+          sendJsonResponse(res, 404, {
+            message: 'locationid not found'
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        if (location.reviews && location.reviews.length > 0) {
+          thisReview = location.reviews.id(req.params.reviewid);
+          if (!thisReview) {
+            sendJsonResponse(res, 404, {
+              message: 'reviewid not found'
+            });
+          } else {
+            thisReview.author = req.body.author;
+            thisReview.rating = req.body.rating;
+            thisReview.reviewText = req.body.reviewText;
+            location.save(function(err, location) {
+              if (err) {
+                sendJsonResponse(res, 404, err);
+              } else {
+                updateAverageRating(location._id);
+                sendJsonResponse(res, 200, thisReview);
+              }
+            });
+          }
+        } else {
+          sendJsonResponse(res, 404, {
+            message: 'No review to update'
+          });
+        }
+      }
+    );
 };
 
 module.exports.reviewsDeleteOne = function(req, res) {
