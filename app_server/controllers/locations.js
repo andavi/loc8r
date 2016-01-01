6e56094563c8ee47468b5202dd0b185b5869763c
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-01-01 09:58:33 -0500

    Add form validation

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -125,7 +125,8 @@ var renderDetailPage = function(req, res, locDetail) {
 var renderReviewForm = function(req, res, locDetail) {
   res.render('location-review-form', {
     title: 'Review ' + locDetail.name + ' on Loc8r',
-    pageHeader: {title: 'Review ' + locDetail.name}
+    pageHeader: {title: 'Review ' + locDetail.name},
+    error: req.query.err
   });
 };
 
@@ -158,14 +159,20 @@ module.exports.doAddReview = function(req, res) {
     method: 'POST',
     json: postdata
   };
-  request(
-    requestOptions,
-    function(err, response, body) {
-      if (response.statusCode === 201) {
-        res.redirect('/location/' + locationid);
-      } else {
-        _showError(req, res, response.statusCode);
+  if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+    res.redirect('/location/' + locationid + '/review/new?err=val');
+  } else {
+    request(
+      requestOptions,
+      function(err, response, body) {
+        if (response.statusCode === 201) {
+          res.redirect('/location/' + locationid);
+        } else if (response.statusCode === 400 && body.name && body.name === 'ValidationError') {
+          res.redirect('/location/' + locationid + '/review/new?err=val');
+        } else {
+          _showError(req, res, response.statusCode);
+        }
       }
-    }
-  );
+    );
+  }
 };
