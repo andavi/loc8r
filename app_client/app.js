c14f7560e624d3333ad218c4ce1d62f806413753
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-01-08 20:02:52 -0500

    Switch routing from express to angular

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,13 @@
 angular.module('loc8rApp', []);
+
+function config ($routeProvider) {
+  $routeProvider
+    .when('/', {
+
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+angular
+  .module('loc8rApp')
+  .config(['$routeProvider', config]);
