ce87e3de1397a066e1aa8bdb7fefd8d8af05b70c
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-01-23 10:47:43 -0500

    Create isolated HTML host page

--- app.js
+++ app.js
@@ -44,9 +44,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app_client')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/api', routesApi);
 app.use('/users', users);
+
+app.use(function(req, res) {
+  res.sendfile(path.join(__dirname, 'app_client', 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
