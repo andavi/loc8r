3c739a31d90a915e1df70807d8c7f6c1b12d2892
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-01-25 16:02:13 -0500

    Use data service to call API and call from detail page controller

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -4,12 +4,21 @@
     .module('loc8rApp')
     .controller('locationDetailCtrl', locationDetailCtrl);
 
-  function locationDetailCtrl () {
+  locationDetailCtrl.$inject = ['$routeParams', 'loc8rData'];
+  function locationDetailCtrl ($routeParams, loc8rData) {
     var vm = this;
+    vm.locationid = $routeParams.locationid;
 
-    vm.pageHeader = {
-      title: 'Location detail page'
-    };
+    loc8rData.locationById(vm.locationid)
+      .success(function(data) {
+        vm.data = {location: data};
+        vm.pageHeader = {
+          title: vm.data.location.name
+        };
+      })
+      .error(function (e) {
+        console.log(e);
+      });
   }
-  
+
 }) ();
