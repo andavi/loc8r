40895200b37e4a84a0db51e8953152872c2bbcf7
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-02-11 05:35:47 -0500

    Add JWT generator

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
   email: {
@@ -15,12 +16,24 @@ var userSchema = new mongoose.Schema({
   salt: String
 });
 
-userSchema.methods.setPasswod = function (password) {
+userSchema.methods.setPassword = function(password) {
   this.salt = crypto.randomBytes(16).toString('hex');
   this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 };
 
-userSchema.methods.validPasswod = function (password) {
+userSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, process.env.JWT_SECRET);
 };
