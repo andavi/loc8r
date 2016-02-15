527d5e8556557d98fc006bb6bed2bf288bae64c8
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-02-15 09:30:32 -0500

    Register authentication service in app

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var appClientFiles = [
   'app_client/reviewModal/reviewModal.controller.js',
   'app_client/common/services/geolocation.service.js',
   'app_client/common/services/loc8rData.service.js',
+  'app_client/common/services/authentication.service.js',
   'app_client/common/filters/formatDistance.filter.js',
   'app_client/common/filters/addHtmlLineBreaks.filter.js',
   'app_client/common/directives/ratingStars/ratingStars.directive.js',
