0c3d4ac07de26ab37491c31188cae08314d6ddbf
Andrew Davis <andrew.bradley.davis@gmail.com>
2016-02-15 21:16:32 -0500

    Remove name field from review modal

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -20,7 +20,6 @@
 
     vm.doAddReview = function (locationid, formData) {
       loc8rData.addReviewById(locationid, {
-        author: formData.name,
         rating: formData.rating,
         reviewText: formData.reviewText
       })
@@ -35,7 +34,7 @@
 
     vm.onSubmit = function () {
       vm.formError = '';
-      if (!vm.formData || !vm.formData.name || !vm.formData.rating || !vm.formData.reviewText) {
+      if (!vm.formData || !vm.formData.rating || !vm.formData.reviewText) {
         vm.formError = 'All fields required, please try again';
         return false;
       } else {
